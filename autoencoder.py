641e4997bfffcf32dc7135cd453f88ce1dee1a54
Chong Tang <ct1y23@soton.ac.uk>
2023-07-23 22:17:14 +0100

    More complex structure

--- autoencoder.py
+++ autoencoder.py
@@ -8,18 +8,30 @@ class Autoencoder(nn.Module):
         super(Autoencoder, self).__init__()
         self.STE = STE
         self.apply_sigmoid = apply_sigmoid
+
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 12, kernel_size=5),
+            nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1),  # B,  32, 16, 16
             nn.ReLU(),
-            nn.Conv2d(12, 24, kernel_size=5),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),  # B,  64, 8, 8
             nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # B, 128, 4, 4
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
         )
+        
         if apply_sigmoid:
             self.encoder.add_module('Sigmoid', nn.Sigmoid())
+
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(24, 12, kernel_size=5),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # B,  64, 8, 8
             nn.ReLU(),
-            nn.ConvTranspose2d(12, 3, kernel_size=5),
+            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),  # B,  32, 16, 16
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),  # B,  3, 32, 32
             nn.Sigmoid(),
         )
 
