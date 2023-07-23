3115b3f00251c158907030f04bb221395450abb2
Chong Tang <ct1y23@soton.ac.uk>
2023-07-23 20:55:06 +0100

    packed code into class

--- main.py
+++ main.py
@@ -10,75 +10,95 @@ from torchvision import datasets, transforms
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_cifar10(batch_size):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-    ])
-    trainset = datasets.CIFAR10('data', train=True, download=True, transform=transform)
-    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
-    testset = datasets.CIFAR10('data', train=False, download=True, transform=transform)
-    testloader = DataLoader(testset, batch_size=batch_size, shuffle=True)
-    return trainloader, testloader
+class AEBinarizerTrainer:
+    def __init__(self, model, lr=1e-3, device=None, model_path=None):
+        self.model = model
+        self.criterion = nn.MSELoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        self.device = device if device else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model.to(self.device)
+        
+        if model_path:
+            self.load_model(model_path)
 
-def train(model, trainloader, testloader, epochs, lr, device):
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    train_losses = []
-    test_losses = []
-    for epoch in range(epochs):
-        train_loss = 0
-        for images, _ in trainloader:
-            images = images.to(device)
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, images)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-        train_loss /= len(trainloader)
-        train_losses.append(train_loss)
-        test_loss = 0
-        for images, _ in testloader:
-            images = images.to(device)
-            outputs = model(images)
-            loss = criterion(outputs, images)
-            test_loss += loss.item()
-        test_loss /= len(testloader)
-        test_losses.append(test_loss)
-        print(f'Epoch {epoch + 1}/{epochs} - train loss: {train_loss:.4f} - test loss: {test_loss:.4f}')
-    return train_losses, test_losses
+    def load_cifar10(self, batch_size):
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+        trainset = datasets.CIFAR10('data', train=True, download=True, transform=transform)
+        self.trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+        testset = datasets.CIFAR10('data', train=False, download=True, transform=transform)
+        self.testloader = DataLoader(testset, batch_size=batch_size, shuffle=True)
 
-def plot_losses(train_losses, test_losses):
-    '''
-    Plot the training and test losses.
-    '''
-    plt.plot(train_losses, label='train loss')
-    plt.plot(test_losses, label='test loss')
-    plt.legend()
-    plt.show()
+    def train(self, epochs):
+        self.model.train()
+        train_losses = []
+        test_losses = []
+        for epoch in range(epochs):
+            train_loss = 0
+            for images, _ in self.trainloader:
+                images = images.to(self.device)
+                self.optimizer.zero_grad()
+                outputs = self.model(images)
+                loss = self.criterion(outputs, images)
+                loss.backward()
+                self.optimizer.step()
+                train_loss += loss.item()
+            train_loss /= len(self.trainloader)
+            train_losses.append(train_loss)
 
-def plot_images(model, testloader, device):
-    images, _ = next(iter(testloader))
-    images = images.to(device)
-    outputs = model(images)
-    images = images.cpu().numpy()
-    outputs = outputs.cpu().detach().numpy()
-    for i in range(10):
-        plt.subplot(2, 10, i + 1)
-        plt.imshow(np.transpose(images[i], (1, 2, 0)))
-        plt.subplot(2, 10, i + 11)
-        plt.imshow(np.transpose(outputs[i], (1, 2, 0)))
-    plt.show()
+            test_loss = 0
+            self.model.eval()
+            with torch.no_grad():
+                for images, _ in self.testloader:
+                    images = images.to(self.device)
+                    outputs = self.model(images)
+                    loss = self.criterion(outputs, images)
+                    test_loss += loss.item()
+            test_loss /= len(self.testloader)
+            test_losses.append(test_loss)
+            self.model.train()
+
+            print(f'Epoch {epoch + 1}/{epochs} - train loss: {train_loss:.4f} - test loss: {test_loss:.4f}')
+        return train_losses, test_losses
+
+    def plot_losses(self, train_losses, test_losses):
+        plt.plot(train_losses, label='train loss')
+        plt.plot(test_losses, label='test loss')
+        plt.legend()
+        plt.show()
+
+    def plot_images(self):
+        images, _ = next(iter(self.testloader))
+        images = images.to(self.device)
+        outputs = self.model(images)
+        images = images.cpu().numpy()
+        outputs = outputs.cpu().detach().numpy()
+        for i in range(10):
+            plt.subplot(2, 10, i + 1)
+            plt.imshow(np.transpose(images[i], (1, 2, 0)))
+            plt.subplot(2, 10, i + 11)
+            plt.imshow(np.transpose(outputs[i], (1, 2, 0)))
+        plt.show()
+
+    def save_model(self, path):
+        torch.save(self.model.state_dict(), path)
+
+    def load_model(self, path):
+        self.model.load_state_dict(torch.load(path))
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'Using device "{device}"')
-    trainloader, testloader = load_cifar10(128)
-    model = Autoencoder(BinarizeSTE, apply_sigmoid=True).to(device)
-    train_losses, test_losses = train(model, trainloader, testloader, epochs=10, lr=1e-3, device=device)
-    plot_losses(train_losses, test_losses)
-    plot_images(model, testloader, device)
+
+    trainer = AEBinarizerTrainer(Autoencoder(BinarizeSTE, apply_sigmoid=True), lr=1e-3, device=device, model_path='model.pth')
+    trainer.load_cifar10(128)
+    train_losses, test_losses = trainer.train(epochs=10)
+    trainer.plot_losses(train_losses, test_losses)
+    trainer.plot_images()
+    trainer.save_model('model.pth')  # Save the trained model
+
 
 if __name__ == '__main__':
     main()
